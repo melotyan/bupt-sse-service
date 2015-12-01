1539ed47c02779605d8eeed194592797e3f5dca4
hao.yan <hao.yan@dajie-inc.com>
2015-12-01 18:51:52 +0800

    配置mybatis

---
+++ src/test/java/cn/sse/bupt/service/UserServiceTest.java
@@ -0,0 +1,14 @@
+package cn.sse.bupt.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by hao.yan on 2015/12/1.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:app_config/bupt-sse-service.xml"})
+public class UserServiceTest {
+
+}
