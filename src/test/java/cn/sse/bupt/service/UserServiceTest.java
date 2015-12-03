22c7027a39a2218a85f21848ff1531e508add664
hao.yan <hao.yan@dajie-inc.com>
2015-12-03 14:03:21 +0800

    添加logback日志文件

--- src/test/java/cn/sse/bupt/service/UserServiceTest.java
+++ src/test/java/cn/sse/bupt/service/UserServiceTest.java
@@ -3,6 +3,8 @@ package cn.sse.bupt.service;
 import cn.sse.bupt.model.UserModel;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,11 +15,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:app_config/bupt-sse-service.xml"})
 public class UserServiceTest {
+    private final static Logger LOGGER = LoggerFactory.getLogger(UserServiceTest.class);
+
     @Autowired
     private UserService userService;
 
     @Test
     public void testInsert() {
+        LOGGER.info("test log");
     }
 
 }
